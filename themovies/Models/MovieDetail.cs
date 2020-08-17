e19f1bb074047b6bb4987db56c33d2faaa7a92fa
Guillermo Meza Fernandez <gmezaf@scientia.digital>
2020-08-17 14:16:42 -0500

    Se agrega archivo faltante

---
+++ themovies/Models/MovieDetail.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace themovies.Models
+{
+    public class MovieDetail : Movie
+    {
+        [JsonProperty("belongs_to_collection")]
+        public object BelongsToCollection { get; set; }
+
+        [JsonProperty("budget")]
+        public int Budget { get; set; }
+
+        [JsonProperty("genres")]
+        public List<Genre> Genres { get; set; }
+
+        [JsonProperty("homepage")]
+        public string Homepage { get; set; }
+
+        [JsonProperty("imdb_id")]
+        public string ImdbId { get; set; }
+
+        [JsonProperty("production_companies")]
+        public List<ProductionCompany> ProductionCompanies { get; set; }
+
+        [JsonProperty("production_countries")]
+        public List<ProductionCountry> ProductionCountries { get; set; }
+
+        [JsonProperty("revenue")]
+        public int Revenue { get; set; }
+
+        [JsonProperty("runtime")]
+        public int Runtime { get; set; }
+
+        [JsonProperty("spoken_languages")]
+        public List<SpokenLanguage> SpokenLanguages { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("tagline")]
+        public string Tagline { get; set; }
+
+        public string Studio
+        {
+            get
+            {
+                List<string> names = new List<string>();
+                foreach (var item in ProductionCompanies)
+                {
+                    names.Add(item.Name);
+                }
+                return string.Join(", ", names);
+            }
+        }
+
+        public string Genre
+        {
+            get
+            {
+                List<string> genres = new List<string>();
+                foreach (var item in Genres)
+                {
+                    genres.Add(item.Name);
+                }
+                return string.Join(", ", genres);
+            }
+        }
+    }
+
+    public class Genre
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class ProductionCompany
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("logo_path")]
+        public string LogoPath { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("origin_country")]
+        public string OriginCountry { get; set; }
+    }
+
+    public class ProductionCountry
+    {
+        [JsonProperty("iso_3166_1")]
+        public string Iso31661 { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class SpokenLanguage
+    {
+        [JsonProperty("iso_639_1")]
+        public string Iso6391 { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
